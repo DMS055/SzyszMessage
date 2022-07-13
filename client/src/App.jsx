6f5eebfb3cb953d8ffae4fb0e4006d6470950214
D.M.S <thebest755p@gmail.com>
2022-07-13 14:19:35 +0200

    add auth component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,20 @@ import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 //require('dotenv').config();
 
-import { ChannelListContainer, ChannelContainer, ChannelSearch } from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
 import './App.css';
 
-const apikey = '';
+const apikey = 'dbnumhfr4qqn';
 
 const client = StreamChat.getInstance(apikey);
 
+const authToken = false;
+
 const App = () => {
+  
+  if (!authToken) return <Auth />
+
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme="team light">
