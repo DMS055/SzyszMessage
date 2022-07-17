a33fc86cc91077b00657185cf18e65df1e94736f
D.M.S <thebest755p@gmail.com>
2022-07-17 05:40:49 +0200

    Added logout functionality

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -5,9 +5,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 
-const SideBar = () => (
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => (
   <div className='channel-list__sidebar'>
-    <div className='channel-list__sidebar__icon1'>
+    <div className='channel-list__sidebar__icon1' onClick={logout}> 
       <div className='icon1__inner'>
           <i class="fa-solid fa-arrow-right-to-bracket"></i>
         </div>
@@ -22,9 +24,21 @@ const Header = () => (
 )
 
 const ChannelListContainer = () => {
+  const logout = () => {
+    cookies.remove('token');
+    cookies.remove("userId");
+		cookies.remove("username");
+		cookies.remove("fullName");
+		cookies.remove("avatarURL");
+    cookies.remove("hashedPassword");
+		cookies.remove("phoneNumber");
+
+    window.location.reload();
+  }
+
   return (
     <>
-      <SideBar />
+      <SideBar logout={logout} />
       <div className='channel-list__list__wrapper'>
         <Header />
         <ChannelSearch />
