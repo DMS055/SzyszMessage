f1a0aaf4657be06d7b34bb67e602ca85d8d066b5
D.M.S <thebest755p@gmail.com>
2022-07-17 06:09:01 +0200

    Renamed `TeamMessage.jsx`

--- client/src/components/TeamMessages.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TeamMessages = () => {
-  return (
-    <div>TeamMessages</div>
-  )
-}
-
-export default TeamMessages
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,5 +6,5 @@ export { default as TeamChannelPreview } from './TeamChannelPreview';
 export { default as ChannelInner } from "./ChannelInner";
 export { default as CreateChannel } from './CreateChannel';
 export { default as EditChannel } from './EditChannel';
-export { default as TeamMessages } from './TeamMessages';
+export { default as TeamMessage } from './TeamMessage';
 export { default as Auth } from './Auth';
