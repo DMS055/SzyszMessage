1c3ec4cbef69976dbcd9d9a90cda78e79f983d74
D.M.S <thebest755p@gmail.com>
2022-07-17 05:02:56 +0200

    fixed server issue

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const authRoutes = require('./routes/auth.js');
+const authRoutes = require("./routes/auth.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.get("/", (req, res) => {
+	res.send("Hello, World!");
 });
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
